20cd3373c3e4710c726aadd2783771029dafd69f
Abdullahi Ayantayo <ayantayoabdullah@gmail.com>
2025-04-05 10:02:43 -0400

    Add Vercel configuration and ESLint settings

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,9 @@ const nextConfig: NextConfig = {
     domains: ['images.unsplash.com'],
   },
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
